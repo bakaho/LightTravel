2998f3d713b0fb2c1b3d22da6ae246d06083b6e8
bakaho <cleariqd@gmail.com>
2019-02-27 17:38:35 +0800

    server

---
+++ Assets/Script/moveControl.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveControl : MonoBehaviour
+{
+
+    public float xDel, yDel;
+    public Camera myCam;
+    bool moveAllow = false;
+    Touch initTouch;
+    Vector3 initMouse;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        myCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            initMouse = Input.mousePosition;
+            moveAllow = true;
+
+            //Ray ray = uiCam.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit = new RaycastHit();
+            //if (Physics.Raycast(ray, out hit))
+            //{
+            //    if (hit.transform.gameObject == this.gameObject)
+            //    {
+            //        print("hit!");
+            //        rotateAllow = true;
+            //    }
+            //}
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float xMoved = Input.mousePosition.x - initMouse.x;
+            float yMoved = Input.mousePosition.y - initMouse.y;
+            //float distance = Mathf.Sqrt((xMoved * xMoved) + (xMoved * xMoved));
+            if (moveAllow)
+            {
+
+                if (xMoved > 50)
+                {
+                    xDel = 2;
+                }
+                else if (xMoved < -50)
+                {
+                    xDel = -2;
+                }
+                else
+                {
+                    xDel = xMoved / 50f;
+                }
+
+                if (yMoved > 50)
+                {
+                    yDel = 2;
+                }
+                else if (yMoved < -50)
+                {
+                    yDel = -2;
+                }
+                else
+                {
+                    yDel = yMoved / 50f;
+                }
+
+            }
+
+        }
+        //if let go, start over again
+        else if (Input.GetMouseButtonUp(0))
+        {
+            moveAllow = false;
+        }
+
+
+    }
+
+    public float Horizontal()
+    {
+        if (moveAllow)
+        {
+            print("0");
+            return xDel;
+
+        }
+        else
+        {
+            //print("1");
+            return Input.GetAxis("Horizontal");
+
+        }
+    }
+
+    public float Vertical()
+    {
+        if (moveAllow)
+        {
+            //print("2");
+            return yDel;
+
+        }
+        else
+        {
+            //print("3");
+            return Input.GetAxis("Vertical");
+
+        }
+    }
+}
--- Assets/Script/myLight.cs
+++ Assets/Script/myLight.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class myLight : MonoBehaviour {
 
+    //move
+    public moveControl joystick;
+    public float speed = 15;
+    //cam
+    public Transform CameraTransform;
+    private Vector3 cameraOffset;
+
+    //
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
 	// Use this for initialization
 	void Start () {
-		
+        joystick = GameObject.FindWithTag("joystick").GetComponent<moveControl>();
+
+        //initialization of camera
+        CameraTransform = GameObject.FindWithTag("MainCamera").transform;
+        cameraOffset = CameraTransform.transform.position - new Vector3(0f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        float h = joystick.Horizontal();
+        float v = joystick.Vertical();
+        bool move = (Mathf.Abs(v - 0) > 0.01f) || (Mathf.Abs(h - 0) > 0.01f);
+        if(move){
+            //Vector3 newPos = transform.position + new Vector3(speed * h * Time.deltaTime, 0, speed * v * Time.deltaTime);
+            transform.position += new Vector3(speed * h * Time.deltaTime, 0, speed * v * Time.deltaTime);
+
+        }
+        Vector3 newCamPos = transform.position + cameraOffset;
+        CameraTransform.position = Vector3.Slerp(CameraTransform.position, newCamPos, smoothFactor);
+
+        //transform.position += new Vector3(speed * h * Time.deltaTime, 0, speed * v * Time.deltaTime);
+
+        //float fXMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
+        //float fZMove = Input.GetAxis("Vertical") * Time.deltaTime * 150f;
+
+        //transform.Translate(fXMove, 0, fZMove);
 		
 	}
 }
